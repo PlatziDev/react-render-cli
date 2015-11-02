79cf75f6250ecc7c19680da985f1667b0b322a84
Sergio Daniel Xalambrí <daniel@platzi.com>
2015-11-01 19:29:37 -0500

    [update] use react.js version

--- app/index.js
+++ app/index.js
@@ -4,15 +4,13 @@ const program = require('commander');
 const render = require('./lib/render');
 
 program
-  .version('1.0.0')
+  .version('0.14.1')
   .usage('[component path]', 'Component to render path')
   .option('-p, --props [props]', 'Props to pass into the component (JSON string)')
   .option('-s, --static [static]', 'If the markup is static (without data-reactid)')
   .parse(process.argv);
 
 const componentPath = path.resolve(process.cwd(), program.args[0]);
-
-require('babel/register');
 
 const data = {
   Component: require(componentPath),
